6bdba0b91efb3670fd5cc2edcf1a1f68ad661a9f
Subash Pradahan <suprowilde5@gmail.com>
2020-07-29 13:00:37 +0200

    Working proper Css (WIP)

---
+++ src/components/Home/view.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../../styles/home.css'
+
+export default function View() {
+  return (
+    <div className="home-container">
+      <div className='text'>
+        Manage Your Projects
+      </div>
+    </div>
+  )
+}
