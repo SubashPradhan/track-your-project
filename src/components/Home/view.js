ce5a790a072f4d7795ed97a6192d26d2cb8ed163
Subash Pradahan <suprowilde5@gmail.com>
2020-08-05 15:00:06 +0200

    Navbar animation on progress

--- src/components/Home/view.js
+++ src/components/Home/view.js
@@ -1,14 +1,18 @@
 import React from 'react'
 import '../../styles/home.css'
 import Login from '../Login'
+import Navbar from '../Navbar'
 
 export default function View() {
   return (
-    <div className="home-container">
-      <div className='text'>
+    <>
+      <Navbar />
+      <div className="home-container">
+        <div className='text'>
           Manage <i>Your</i> Projects
       </div>
-      <Login />
-    </div>
+        <Login />
+      </div>
+    </>
   )
 }
