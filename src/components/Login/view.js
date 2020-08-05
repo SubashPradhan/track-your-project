ab6b3e44fdeddfe0e52a33711fcb7182a08116da
Subash Pradahan <suprowilde5@gmail.com>
2020-08-05 15:09:34 +0200

    Deploy fixes

--- src/components/Login/view.js
+++ src/components/Login/view.js
@@ -2,14 +2,12 @@ import React from 'react'
 import '../../styles/login.css'
 import { TextField, InputAdornment } from '@material-ui/core';
 import { AccountCircle, VisibilityOffRounded } from '@material-ui/icons';
-import logo from '../../assets/images/project-logo.png';
 
 export default function View() {
   return (
     <div>
       <div className='login-container'>
         <form autoComplete='off' className='login-form'>
-          {/* <img className='project-logo' src={logo} alt='project-tracker-logo'/> */}
           <TextField
             type='text'
             className='text-field'
