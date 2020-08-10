452fdf56370652b07c7d93806c7d750433d788bf
Subash Pradahan <suprowilde5@gmail.com>
2020-08-10 16:20:59 +0200

    Bootstrap Implementation

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import store from './store'
 import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.css';
 
 ReactDOM.render(
   <React.StrictMode>
