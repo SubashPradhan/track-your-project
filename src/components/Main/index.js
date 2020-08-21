2c4744de6318e0e34f2c0ee5bedcf620ec89650e
Subash Pradahan <suprowilde5@gmail.com>
2020-08-21 23:13:35 +0200

    Arranging components

---
+++ src/components/Main/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import {View } from './view'
+
+class Main extends Component {
+  render() {
+    return (
+      <div>
+        <View />
+      </div>
+    )
+  }
+}
+
+export default Main
