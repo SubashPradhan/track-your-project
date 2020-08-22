0212e42071de54785a5e896d60d572a8499058ad
Subash Pradahan <suprowilde5@gmail.com>
2020-08-22 21:32:55 +0200

    Scrolling effect on navbar

--- src/components/Main/view.js
+++ src/components/Main/view.js
@@ -2,7 +2,6 @@ import React from 'react'
 import Home from '../Home'
 import About from '../About'
 import Navbar from '../Navbar'
-import Login from '../Login'
 import { Container } from 'react-bootstrap'
 import '../../styles/main.css'
 
@@ -13,7 +12,6 @@ export const View = () => {
       <Container fluid className="main-container">
         <Home />
         <About />
-        <Login />
       </Container>
     </>
   )
