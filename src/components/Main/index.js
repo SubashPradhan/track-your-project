308a5dd755bbeb6cd04260adb5c3ac8f52ad1919
Subash Pradahan <suprowilde5@gmail.com>
2020-09-09 18:19:58 +0200

    Code cleanup and removing scrolling props

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,22 +1,9 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import {View } from './view'
-import {getScrollHeight} from '../../actions/getScrollHeight'
 import {getMenuClickedStat} from '../../actions/menuClicked'
 
 class Main extends Component {
-  state = {
-    aboutRef: React.createRef(), 
-    homeRef: React.createRef(),
-    goalsRef: React.createRef(),
-    contactRef: React.createRef()
-  }
-
-  componentDidMount = () => {
-    const refs = [this.state.homeRef, this.state.aboutRef, this.state.goalsRef, this.state.contactRef]
-    this.props.getScrollHeight(refs)
-  }
-
   handleClick = () => {
     const {menuClicked} = this.props
     if (menuClicked){
@@ -29,10 +16,6 @@ class Main extends Component {
       <div>
         <View 
         menuClicked={this.props.menuClicked} 
-        aboutRef={this.state.aboutRef}
-        homeRef={this.state.homeRef}
-        goalsRef={this.state.goalsRef}
-        contactRef={this.state.contactRef}
         handleClick={this.handleClick}
         />
       </div>
@@ -43,8 +26,7 @@ class Main extends Component {
 const mapStateToProps = state => {
   return{
     menuClicked: state.menuClicked,
-    scrollHeight: state.scrollHeight
   }
 }
 
-export default connect (mapStateToProps, {getScrollHeight, getMenuClickedStat}) (Main)
+export default connect (mapStateToProps, {getMenuClickedStat}) (Main)
--- src/components/Navbar/view.js
+++ src/components/Navbar/view.js
@@ -1,17 +1,16 @@
 import React from 'react'
 import '../../styles/navbar.css'
 import projectLogo from '../../assets/images/project-logo.png'
-import { Link } from 'react-router-dom'
 import { Container, Row } from 'react-bootstrap'
 
 export default function View(props) {
   const { handleMenu, menuClicked, setFocus, scrollNav} = props
 
   const Links = <div className={menuClicked ? 'nav-links nav-active' : 'nav-links'}>
-    <Link to='#home' id="home" className='link' onClick={setFocus}>Home</Link>
-    <Link to='#about' id="about" className='link' onClick={setFocus}>About</Link>
-    <Link to='#goals' id="goals" className='link' onClick={setFocus}>Our Goals</Link>
-    <Link to='#contact' id="contact" className='link' onClick={setFocus}>Contact</Link>
+    <a href="#home" className='link' onClick={setFocus}>Home</a>
+    <a href="#about" className='link' onClick={setFocus}>About</a>
+    <a href="#goals" className='link' onClick={setFocus}>Our Goals</a>
+    <a href="#contact" className='link' onClick={setFocus}>Contact</a>
   </div>
 
   return (
