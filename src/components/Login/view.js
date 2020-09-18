a31f4a4ac46bd69d10221115d7eda498ae1c60aa
Subash Pradahan <suprowilde5@gmail.com>
2020-09-18 23:39:12 +0200

    Adding react reveal on login page

--- src/components/Login/view.js
+++ src/components/Login/view.js
@@ -5,6 +5,8 @@ import { AccountCircle, VisibilityOffRounded } from '@material-ui/icons';
 import { Container, Row, Col } from 'react-bootstrap';
 import LoginImage from '../../assets/images/login.svg'
 import ProjectLogo from '../../assets/images/project-logo.svg'
+import { Flip, Fade, Zoom, LightSpeed} from 'react-reveal';
+import Tada from 'react-reveal/Tada';
 
 export default function View(props) {
   const { handleClick } = props
@@ -13,16 +15,23 @@ export default function View(props) {
       <div className='project-logo-login'>
         <img src={ProjectLogo} alt="project-logo" className="img-fluid" onClick={handleClick} />
       </div>
+
       <Row className="login-content">
         <Col lg={6} className="img-container">
           <div className="login-img">
-            <img src={LoginImage} alt="Login" className="img-fluid"></img>
+            <LightSpeed left>
+              <img src={LoginImage} alt="Login" className="img-fluid"></img>
+            </LightSpeed>
           </div>
         </Col>
 
         <Col lg={6} className="login-form-container">
           <form autoComplete='off' className='login-form'>
-            <h3 className='login-header'> Login to Project Management</h3>
+            
+            <Tada bottom>
+              <h3 className='login-header'> Login to Project Management</h3>
+            </Tada>
+
             <TextField
               type='text'
               className='text-field'
@@ -57,7 +66,6 @@ export default function View(props) {
             </div>
             <button className='login-submit'>Login</button>
           </form>
-          {/* </div> */}
         </Col>
       </Row>
     </Container>
